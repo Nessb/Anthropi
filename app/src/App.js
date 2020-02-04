627268be2b2d9168702369e6eeb55b4d78feab87
nessb <belhadj.nessim@gmail.com>
2020-02-05 00:23:33 +0100

    build

---
+++ app/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Navbar/Navbar.js.js'
+import Presentation from './components/Main/Presentation.js.js'
+import Block from './components/Main/Block.js.js'
+import SimulPhone from './components/Main/SimulPhone.js.js'
+import BlockFormations from './components/Main/BlockFormations.js.js'
+import Partenaires from './components/Main/Partenaires.js.js'
+import Formations from './components/Main/Formations.js.js'
+import Avis from './components/Main/Avis.js.js'
+// import References from './components/Main/References.js'
+import Formateurs from './components/Main/Formateurs.js.js'
+import Contact from './components/Contact/Contact.js.js'
+import Footer from './components/Footer/Footer.js.js'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbreact/dist/css/mdb.css'
+
+const App = props => {
+  const contact = Object.keys(props.data.contact).map(key => (
+    <Contact key={key} details={props.data.contact[key]} />
+  ))
+  console.log(contact)
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <Navbar />
+        <Presentation />
+        <Block />
+        <SimulPhone />
+        <Partenaires />
+        <BlockFormations />
+        <Formations />
+        <Avis />
+        {/* <References /> */}
+        <Formateurs />
+        <Contact details={props.data.contact} />
+      </header>
+      <footer className='App-footer'>
+        <Footer />
+      </footer>
+    </div>
+
+  )
+}
+
+export default App
